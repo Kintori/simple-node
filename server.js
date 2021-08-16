e861c8edb4e374dca707eae0e929db26d6deddfe
KProft <kcp96h@yahoo.com>
2021-08-16 02:15:48 -0500

    updated travis-ci.yaml file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -5,6 +7,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule!');
+	console.log(`My favorite instructor is ${FAVORITE_INSTRUCTOR}`);
     await sleep(5000);
   }
 }
